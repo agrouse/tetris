659ea154b609766cc7b0608155b001471e4db2f3
Vasanth <vasanthrajvr@gmail.com>
2018-02-19 20:44:33 -0600

    Hello

--- tetris_solution.py
+++ tetris_solution.py
@@ -22,7 +22,7 @@ def printInstructions():
     print("Use the arrow keys to move Left, Right, and Down.")
     print("Press Up to rotate counter-clockwise")
     print("Press 'r' to restart.")
-    print("Press 'q' to end the game.")
+    print("Press 'q' to end the game yent.")
 
 def tetrisPieces():
     iPiece = [[ True,  True,  True,  True]]
